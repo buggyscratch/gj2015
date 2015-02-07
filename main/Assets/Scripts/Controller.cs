69845556157df5fe0a0ff8a3d613410da2968c10
AudiologMusic <fuad@vonloops.com>
2015-02-07 16:19:19 +0100

    KLAR TIL CONTROLLER !

--- main/Assets/Scripts/Controller.cs
+++ main/Assets/Scripts/Controller.cs
@@ -2,20 +2,27 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
-
-	public float targetSpeed, speed;
+	public Transform motherShip_t;
+	public Rigidbody2D motherShip_r2d;
+	public float targetSpeed;
+	private float speed;
 	public string axis;
+	public string fire;
 
 	// Use this for initialization
 	void Start () {
 		if (axis.Equals("Vertical")){
 			speed = 180;
 		}
+
+		motherShip_t = GameObject.Find ("Mothership").GetComponent<Transform> ();
+		motherShip_r2d = GameObject.Find ("Mothership").GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		UpdatePosition ();
+		UpdateInteraction ();
 	}
 
 	//Updates rotation speed based on player input and decelaration
@@ -23,5 +30,13 @@ public class Controller : MonoBehaviour {
 	{	
 		speed += Input.GetAxis (axis) * targetSpeed;
 		transform.rotation = Quaternion.Euler(new Vector3 (0f,0f,speed));
+	}
+
+	void UpdateInteraction(){
+		if (Input.GetButtonDown (fire)) {
+			Debug.Log ("fired");
+			Vector2 temp = new Vector2(0,0);	// HVAD SKAL TEMP VÆRE !!!
+			motherShip_r2d.AddForce(temp);
+		}
 	}
 }

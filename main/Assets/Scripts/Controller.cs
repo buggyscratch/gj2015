c6ea9df09b1db63dca6b84b2c8c71a0961362479
Andreas G <andreas.good@gmail.com>
2015-02-07 12:24:00 +0100

    Basic controls

---
+++ main/Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float targetSpeed, speed;
+	public string axis;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdatePosition ();
+	}
+
+	//Updates rotation speed based on player input and decelaration
+	void UpdatePosition()
+	{	
+		speed += Input.GetAxis (axis) * targetSpeed;
+		transform.rotation = Quaternion.Euler(new Vector3 (0f,0f,speed));
+	}
+}

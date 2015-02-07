1d5654da42d813f6f4c8856c620774cecb5c5294
Andreas G <andreas.good@gmail.com>
2015-02-07 17:36:37 +0100

    Motion of the ocean

--- main/Assets/Scripts/Controller.cs
+++ main/Assets/Scripts/Controller.cs
@@ -2,27 +2,25 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
-	public Transform motherShip_t;
+	public Transform motherShip_t, sprite;
 	public Rigidbody2D motherShip_r2d;
-	public float targetSpeed;
+	public float targetSpeed, shotForce;
+	public string axis, fire;
+
 	private float speed;
-	public string axis;
-	public string fire;
 
 	// Use this for initialization
 	void Start () {
 		if (axis.Equals("Vertical")){
 			speed = 180;
 		}
-
-		motherShip_t = GameObject.Find ("Mothership").GetComponent<Transform> ();
-		motherShip_r2d = GameObject.Find ("Mothership").GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		UpdatePosition ();
 		UpdateInteraction ();
+		//FollowMothership ();
 	}
 
 	//Updates rotation speed based on player input and decelaration
@@ -34,9 +32,16 @@ public class Controller : MonoBehaviour {
 
 	void UpdateInteraction(){
 		if (Input.GetButtonDown (fire)) {
-			Debug.Log ("fired");
-			Vector2 temp = new Vector2(0,0);	// HVAD SKAL TEMP VÆRE !!!
-			motherShip_r2d.AddForce(temp);
+			Vector2 temp = new Vector2(motherShip_t.position.x-sprite.position.x,motherShip_t.position.y-sprite.position.y).normalized;
+			motherShip_r2d.AddForce(temp * shotForce);
 		}
+	}
+
+	//Not usable
+	void FollowMothership()	{
+		Vector2 toMothership = new Vector2 (motherShip_t.position.x - transform.position.x, motherShip_t.position.y - transform.position.y);
+		Debug.Log (toMothership);
+		transform.position = motherShip_t.position;
+		//transform.Translate (toMothership*0.001f);
 	}
 }
